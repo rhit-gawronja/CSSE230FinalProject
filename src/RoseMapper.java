0fa8528f88b62c9eaa60c9e69332e33092dbee09
rhit-gawronja <gawronja@rose-hulman.edu>
2022-02-07 08:05:48 -0500

    New visual file for the visual mapper program

--- src/RoseMapper.java
+++ src/RoseMapper.java
@@ -1,23 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.swing.*;
 public class RoseMapper{
     public void createMap(){
-        Jframe map = new JFrame();
+        JFrame map = new JFrame();
     }
     class Map{
         HashMap<String,T> nodeMap;
         Map(){
             this.nodeMap=new HashMap();
         }
+
+    }
+    Class Location{
+       public  int vert;
+         public int horiztonatl;
     }
     //TODO: Add image to the jframe of Rose-hulman map
     //TODO: Determine if we are using A* of Djikstras algo
     //TODO: Create buttons:
     class LocationNode{
         ArrayList<LocationNode> connections;
-        T location;
+        Location location;
         LocationNode(T element){
             this.connections=new ArrayList<LocationNode>();
-            this.location=element
+            this.location=element;
+        }
+        int findDistance(LocationNode target){
+            int y =math.pow(this.location.vert,2);
+            int x=MATH.pow(this.location.horizontal,2);
         }
     }
 }

505610e164f19aecb7b1f9a54adc309c62450fd0
rhit-gawronja <gawronja@rose-hulman.edu>
2022-02-07 21:54:05 -0500

    Added the path class

--- src/RoseMapper.java
+++ src/RoseMapper.java
@@ -1,35 +1,45 @@
-// import java.util.ArrayList;
-// import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-// import javax.swing.*;
-// public class RoseMapper{
-//     public void createMap(){
-//         JFrame map = new JFrame();
-//     }
-//     class Map{
-//         HashMap<String,LocationNode> nodeMap;
-//         Map(){
-//             this.nodeMap=new HashMap();
-//         }
+import javax.swing.*;
+public class RoseMapper{
+    public void createMap(){
+        JFrame map = new JFrame();
+    }
+    class Map{
+        HashMap<String,LocationNode> nodeMap;
+        Map(){
+            this.nodeMap=new HashMap();
+        }
 
-//     }
-//     class Location{
-//        public  int vert;
-//          public int horiztonatl;
-//     }
-//     //TODO: Add image to the jframe of Rose-hulman map
-//     //TODO: Determine if we are using A* of Djikstras algo
-//     //TODO: Create buttons:
-//     class LocationNode{
-//         ArrayList<LocationNode> connections;
-//         Location location;
-//         LocationNode(T element){
-//             this.connections=new ArrayList<LocationNode>();
-//             this.location=element;
-//         }
-//         int findDistance(LocationNode target){
-            
-//         }
-//     }
-// }
-// // This is a test comment.
+    }
+    class Location{
+       public  int vert;
+         public int horiztonatl;
+    }
+    //TODO: Add image to the jframe of Rose-hulman map
+    //TODO: Determine if we are using A* of Djikstras algo
+    //TODO: Create buttons:
+    //locationNode== vertex in djikstras algorithm
+    class Path{
+        public LocationNode destination;
+        public double cost;
+        public Path(LocationNode destination,double cost){
+            this.destination=destination;
+            this.cost=cost;
+        }
+    }
+    class LocationNode{
+        public String name;
+
+        ArrayList<LocationNode> connections;
+        public double cost;
+        public LocationNode prev;
+        public int temp;
+        public LocationNode(String name){
+            this.name=name;
+        }
+        
+    }
+}
+// This is a test comment.

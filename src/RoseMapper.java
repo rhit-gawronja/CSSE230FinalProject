189f2eed3a6916e404d1c49e24016c7b2654fc00
rhit-gawronja <gawronja@rose-hulman.edu>
2022-02-07 20:49:46 -0500

    Added campus map and working through graphics

--- src/RoseMapper.java
+++ src/RoseMapper.java
@@ -1,36 +1,35 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+// import java.util.ArrayList;
+// import java.util.HashMap;
 
-import javax.swing.*;
-public class RoseMapper{
-    public void createMap(){
-        JFrame map = new JFrame();
-    }
-    class Map{
-        HashMap<String,T> nodeMap;
-        Map(){
-            this.nodeMap=new HashMap();
-        }
+// import javax.swing.*;
+// public class RoseMapper{
+//     public void createMap(){
+//         JFrame map = new JFrame();
+//     }
+//     class Map{
+//         HashMap<String,LocationNode> nodeMap;
+//         Map(){
+//             this.nodeMap=new HashMap();
+//         }
 
-    }
-    Class Location{
-       public  int vert;
-         public int horiztonatl;
-    }
-    //TODO: Add image to the jframe of Rose-hulman map
-    //TODO: Determine if we are using A* of Djikstras algo
-    //TODO: Create buttons:
-    class LocationNode{
-        ArrayList<LocationNode> connections;
-        Location location;
-        LocationNode(T element){
-            this.connections=new ArrayList<LocationNode>();
-            this.location=element;
-        }
-        int findDistance(LocationNode target){
-            int y =math.pow(this.location.vert,2);
-            int x=MATH.pow(this.location.horizontal,2);
-        }
-    }
-}
-// This is a test comment.
+//     }
+//     class Location{
+//        public  int vert;
+//          public int horiztonatl;
+//     }
+//     //TODO: Add image to the jframe of Rose-hulman map
+//     //TODO: Determine if we are using A* of Djikstras algo
+//     //TODO: Create buttons:
+//     class LocationNode{
+//         ArrayList<LocationNode> connections;
+//         Location location;
+//         LocationNode(T element){
+//             this.connections=new ArrayList<LocationNode>();
+//             this.location=element;
+//         }
+//         int findDistance(LocationNode target){
+            
+//         }
+//     }
+// }
+// // This is a test comment.
